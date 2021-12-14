284b03414f9cfb01715aa3ab8d670d01e81c2f47
Javier Victoria Mohamed <javiervm@celtiberian.es>
2021-12-14 18:25:18 +0100

    Fix some eslint conflicts with jest

--- ygo-card-search/test/allCardsSlice.test.ts
+++ ygo-card-search/test/allCardsSlice.test.ts
@@ -4,8 +4,21 @@ import { loadCards } from '../src/features/searchBar/allCards/allCardsSlice';
 // import '@babel/polyfill';
 // import store from '../src/features/searchBar/store';
 
+// interface Card {
+//   id: string;
+//   name: string;
+//   type: string;
+// }
+
+// const initialState = {
+//   cards: [],
+//   isLoading: false,
+//   hasError: false
+// }
+
 describe('Tests for all cards actions and reducer', () => {
   test('Should load all the cards', async () => {
-    expect(loadCards().length).toBe(11655);
+    // console.log(loadCards());
+    expect(loadCards().length === 11655).toBe(true);
   });
 });
