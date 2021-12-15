fc763e2596aa5a4a326c5ee27bdd7f548a305977
Javier Victoria Mohamed <javiervm@celtiberian.es>
2021-12-15 19:47:52 +0100

    Started integrating backend with the DB in MongoDB using mongoose

---
+++ ygo-card-search/express/routes/auth.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+router.post('/register', async (req, res) => {
+
+    const user = new User (({
+        name: req.body.name,
+        email: req.body.email,
+        password: password
+    }));
+
+    try {
+        const savedUser = await user.save();
+        res.json({
+            error: null,
+            data: savedUser
+        })
+    } catch (error) {
+        res.status(400).json({error})
+    }
+});
+
+module.exports = router;
--- ygo-card-search/express/server.js
+++ ygo-card-search/express/server.js
@@ -1,19 +1,89 @@
+require('dotenv').config();
 const express = require('express'),
+    app = express();
+      
+const jwt = require('jsonwebtoken'),
     bodyParser = require('body-parser'),
-    jwt = require('jsonwebtoken'),
-    config = require('./configs/config'),
-    app = express(),
-    port = process.env.PORT || 5000;
+    mongoose = require('mongoose');
 
+const port = process.env.PORT || 5000;
+const authRoutes = require('./routes/auth');
 
-app.set('pass', config.pass);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const uri = `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@cluster0.iwfvn.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+const option = { useNewUrlParser: true, useUnifiedTopology: true };
 
+console.log(process.env.USERDB);
+console.log(process.env.PASSWORD);
+console.log(process.env.DBNAME);
 
+mongoose.connect(uri, option)
+.then(() => console.log('Database conected'))
+.catch(e => console.log('db error:', e));
 
 app.listen(port, () => console.log(`Listening on port ${port}'`));
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
 });
+
+
+// Middlewares
+app.use('/api/user', authRoutes);
+
+
+
+// app.get('/auth', (req, res) => {
+//     res.sendFile('auth.html', {root: `${__dirname}/views/`});
+// })
+
+// app.post('/login', (req, res) => {
+//     if(req.body.user === "default" && req.body.pass === "defaultpass") {
+//         const payload = {
+//             check: true
+//         }
+
+//         const token = jwt.sign(payload, app.get('pass'), {
+//             expiresIn: 1440
+//         });
+
+//         res.json({
+//             message: 'Authentication completed',
+//             token: token
+//         });
+
+//     } else {
+//         res.json({ message: "You have the password or user not correct" })
+//     }
+// });
+
+// const protectedRoutes = express.Router();
+
+// protectedRoutes.use((req, res, next) => {
+//     const token = req.headers['access-token'];
+
+//     if(token) {
+//         jwt.verify(token, app.get('pass'), (err, decoded) => {
+//             if(err) {
+//                 return res.json({ message: 'Token not valid' });
+//             } else {
+//                 req.decoded = decoded;
+//                 next();
+//             }
+//         });
+//     } else {
+//         res.send({
+//             message: 'Token not provided'
+//         });
+//     }
+// });
+
+
+// app.get('/data', protectedRoutes, (req, res) => {
+//     const data = [
+//         {id: 1, name: "default"},
+//         {id: 2, name: "admin"},
+//         {id: 3, name: "Javier"}
+//     ];
+
+//     res.json(data);
+// });
